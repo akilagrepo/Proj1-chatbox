9c8cc750144d90c299f60301acab46bfad43e81e
akilagrepo <akilaag@gmail.com>
2026-05-13 15:00:31 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ak-11052026" # Replace with your actual S3 bucket name
+    bucket = "akila130526" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
